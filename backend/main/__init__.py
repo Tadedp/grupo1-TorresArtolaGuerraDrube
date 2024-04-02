3f54ef400ea39e01a42c364c6c5bedfc80672803
valeartola <mv.artola@alumno.um.edu.ar>
2024-04-02 11:21:08 -0300

    TP2

---
+++ backend/main/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from dotenv import load_dotenv
+from flask_restful import Api
+import main.resources as resources
+
+api = Api()
+
+def create_app():
+    app = Flask(__name__)
+    load_dotenv()
+    
+    api.add_resource(resources.AnimalesResources, "/animales")
+    
+    api.add_resource(resources.AnimalResources, "/animal/<id>")
+    
+    api.init_app(app)
+    
+    return app
