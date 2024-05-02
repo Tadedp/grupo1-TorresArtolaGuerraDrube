f3cb2cebc3cab4139627fa48ab676efb01a60c99
celi <celi@Ubuntu.myguest.virtualbox.org>
2024-05-02 11:34:28 -0300

    prestamos filtro y paginación

--- backend/main/resources/prestamos.py
+++ backend/main/resources/prestamos.py
@@ -2,12 +2,45 @@ from flask_restful import Resource
 from flask import request, jsonify
 from main.models import PrestamoModel, LibroModel
 from .. import db
+from sqlalchemy import func, desc, asc
 
 class Prestamos(Resource):
     def get(self):
-        prestamos = db.session.query(PrestamoModel).all()
-        return jsonify([prestamo.to_json() for prestamo in prestamos])
+        page = 1
+        per_page = 10
+        prestamos = db.session.query(PrestamoModel)
+        
+        if request.args.get('page'):
+            page = int(request.args.get('page'))
+        if request.args.get('per_page'):
+            per_page = int(request.args.get('per_page'))
+
+        if request.args.get('fecha_inicio'):
+            prestamos=prestamos.filter(PrestamoModel.fecha_inicio.like("%"+request.args.get('fecha_inicio')+"%"))
+                    
+        if request.args.get('fecha_fin'):
+            prestamos=prestamos.filter(PrestamoModel.fecha_fin.like("%"+request.args.get('fecha_fin')+"%"))
+                    
+        if request.args.get('id_usuario'):
+            prestamos=prestamos.filter(PrestamoModel.id_usuario.like("%"+request.args.get('id_usuario')+"%"))
+
+        if request.args.get('sortby_nrLibros'):
+            if request.args.get('sortby_nrLibros') == "asc":
+                prestamos=prestamos.outerjoin(PrestamoModel.libros).group_by(PrestamoModel.id).order_by(func.count(LibroModel.id).asc())
+            if request.args.get('sortby_nrLibros') == "desc":
+                prestamos=prestamos.outerjoin(PrestamoModel.libros).group_by(PrestamoModel.id).order_by(func.count(LibroModel.id).desc())
+
+
+        prestamos = prestamos.paginate(page=page, per_page=per_page, error_out=True)
     
+        return jsonify({'prestamos': [prestamo.to_json() for prestamo in prestamos],
+                'total': prestamos.total,
+                'pages': prestamos.pages,
+                'page': page
+                })
+
+
+
     def post(self):
         libros_ids = request.get_json().get('libros')
         prestamo = PrestamoModel.from_json(request.get_json())
@@ -25,6 +58,8 @@ class Prestamos(Resource):
             return "Formato de datos incorrecto.", 400
         return prestamo.to_json(), 201 
             
+
+
 class Prestamo(Resource):
     def get(self, id):
         try:
