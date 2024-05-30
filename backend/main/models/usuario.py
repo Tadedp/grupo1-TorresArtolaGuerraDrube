0fc9d81ee36cea4ed2d4e946c0d012d787bb6750
Tadedp <tadeodp22@gmail.com>
2024-05-30 11:44:57 -0300

    TP6

--- backend/main/models/usuario.py
+++ backend/main/models/usuario.py
@@ -1,19 +1,31 @@
 from .. import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Usuario(db.Model):
     __tablename__ = "usuarios"
     id = db.Column(db.Integer, primary_key = True)
     nombre = db.Column(db.String(100), nullable = False)
     apellido = db.Column(db.String(100), nullable = False)
-    mail = db.Column(db.String(100), nullable = False)
+    mail = db.Column(db.String(100), unique=True, index=True, nullable = False)
     dni = db.Column(db.Integer,nullable = False)
     telefono = db.Column(db.Integer,nullable = False)
-    rol = db.Column(db.String(100),nullable = False)
+    rol = db.Column(db.String(100),nullable = False, server_default = "Usuario")
     alias = db.Column(db.String,nullable = False)
     contraseña = db.Column(db.String,nullable = False)
     reseñas = db.relationship("Reseña", back_populates="usuario",cascade="all, delete-orphan")
     prestamos = db.relationship("Prestamo", back_populates="usuario",cascade="all, delete-orphan")
     notificaciones = db.relationship("Notificacion", back_populates="usuario",cascade="all, delete-orphan")
+
+    @property
+    def plain_contraseña(self):
+        raise AttributeError('La contraseña no puede ser leida.')
+    
+    @plain_contraseña.setter
+    def plain_contraseña(self, contraseña):
+        self.contraseña = generate_password_hash(contraseña)
+    
+    def validar_contraseña(self,contraseña):
+        return check_password_hash(self.contraseña, contraseña)
 
     def __repr__(self):
         return '<Usuario> id:%r, nombre:%r, apellido:%r' % (self.id, self.nombre, self.apellido)
@@ -27,8 +39,7 @@ class Usuario(db.Model):
             'dni': self.dni,
             'telefono': self.telefono,
             'rol': str(self.rol),
-            'alias': str(self.alias),
-            'contraseña': str(self.contraseña)    
+            'alias': str(self.alias)   
         }
         return usuario_json
 
@@ -45,7 +56,6 @@ class Usuario(db.Model):
             'telefono': self.telefono,
             'rol': str(self.rol),
             'alias': str(self.alias),
-            'contraseña': str(self.contraseña),
             'notificaciones':notificaciones,
             'prestamos':prestamos,
             'reseñas':reseñas  
@@ -57,8 +67,7 @@ class Usuario(db.Model):
             'id': self.id,
             'nombre': str(self.nombre),
             'apellido': str(self.apellido),
-            'alias': str(self.alias),
-            'contraseña': str(self.contraseña)
+            'alias': str(self.alias)
         }
         return usuario_json
 
@@ -73,7 +82,6 @@ class Usuario(db.Model):
         rol = usuario_json.get('rol')
         alias = usuario_json.get('alias')
         contraseña = usuario_json.get('contraseña')
-
         return Usuario(id = id,
                        nombre = nombre,
                        apellido = apellido,
@@ -82,5 +90,5 @@ class Usuario(db.Model):
                        telefono = telefono,
                        rol = rol,
                        alias = alias,
-                       contraseña = contraseña  
+                       plain_contraseña = contraseña  
                     )
